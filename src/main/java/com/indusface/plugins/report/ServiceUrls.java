ff2c4fe341933e8846f908abd6806fc50aa44f6a
productteam-was <product-team@indusface.com>
2024-12-10 17:51:25 +0530

    prod

--- src/main/java/com/indusface/plugins/report/ServiceUrls.java
+++ src/main/java/com/indusface/plugins/report/ServiceUrls.java
@@ -1,7 +1,7 @@
 package com.indusface.plugins.report;
 
 public class ServiceUrls {
-    public static final String SCAN_STATUS_API = "http://wastest.indusface.com/jenkins/v1/scan-status/%s";
-    public static final String SCAN_REPORT_API = "http://wastest.indusface.com/jenkins/v1/scan-details/%s";
-    public static final String SCAN_START_API = "http://wastest.indusface.com/jenkins/v1/start-scan";
+    public static final String SCAN_STATUS_API = "https://was.indusface.com/jenkins/v1/scan-status/%s";
+    public static final String SCAN_REPORT_API = "https://was.indusface.com/jenkins/v1/scan-details/%s";
+    public static final String SCAN_START_API = "https://was.indusface.com/jenkins/v1/start-scan";
 }
